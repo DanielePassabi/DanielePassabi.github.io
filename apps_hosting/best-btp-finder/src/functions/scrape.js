1dea57ea6fc92da7c357ddbbfb3f4e3c784028bb
Daniele <daniele.passabi@hpa.ai>
2023-09-23 15:42:37 +0200

    Update scrape.js

--- apps_hosting/best-btp-finder/src/functions/scrape.js
+++ apps_hosting/best-btp-finder/src/functions/scrape.js
@@ -63,29 +63,66 @@ async function parseHtmlTableToDataFrame() {
 }
 
 function applyDataTransformations(df) {
+  
+  console.log('Input df:')
+  console.log(df)
+
   console.log('[scrape.js] applyDataTransformations() function starts')
-  df['prezzo'] = df['prezzo'].map(val => parseFloat(val.replace(',', '.')));
-  df['durata'] = df['durata'].map(val => parseFloat(val.replace(',', '.')));
-  df['tasso'] = df['tasso'].map(val => parseFloat(val.replace(',', '.').replace('%', '')));
-  df['cedolaNetta'] = df['tasso'].map((val, i) => (100 / df['prezzo'][i]) * val * 0.875);
-  df['rendimentoNetto'] = df['tasso'].map((val, i) => (((100 / df['prezzo'][i]) - 1) * 100 / df['durata'][i]) * 0.875 + df['cedolaNetta'][i]);
+  
+  df._data['prezzo'] = df._data['prezzo'].map(val => parseFloat(val.replace(',', '.')));
+  console.log("[scrape.js] - 'prezzo' mapped correctly")
+  
+  df._data['durata'] = df._data['durata'].map(val => parseFloat(val.replace(',', '.')));
+  console.log("[scrape.js] - 'durata' mapped correctly")
+  
+  df._data['tasso'] = df._data['tasso'].map(val => parseFloat(val.replace(',', '.').replace('%', '')));
+  console.log("[scrape.js] - 'tasso' mapped correctly")
+  
+  df._data['cedolaNetta'] = df._data['tasso'].map((val, i) => (100 / df._data['prezzo'][i]) * val * 0.875);
+  console.log("[scrape.js] - 'cedolaNetta' mapped correctly")
+  
+  df._data['rendimentoNetto'] = df._data['tasso'].map((val, i) => (((100 / df._data['prezzo'][i]) - 1) * 100 / df._data['durata'][i]) * 0.875 + df._data['cedolaNetta'][i]);
+  console.log("[scrape.js] - 'rendimentoNetto' mapped correctly")
+
   console.log('[scrape.js] applyDataTransformations() function ends')
+
+  console.log('Updated df:')
+  console.log(df)
+
   return df;
 }
 
 function findBestBtps(df, minCedola, minRendimento, minDurata, maxDurata, maxPrezzo) {
+  
+  console.log('[scrape.js] findBestBtps() function starts');
 
-  console.log('[scrape.js] findBestBtps() function starts')
-  const filteredDf = df.filter(
-    row => row['cedolaNetta'] > minCedola &&
-      row['durata'] > minDurata &&
-      row['durata'] < maxDurata &&
-      row['prezzo'] < maxPrezzo &&
-      row['rendimentoNetto'] > minRendimento
-  );
-  const sortedDf = filteredDf.sortBy('rendimentoNetto', false);
-  console.log('[scrape.js] findBestBtps() function ends')
-  return sortedDf;
+  const numRows = df._data['cedolaNetta'].length; // Assuming all columns have the same number of rows
+  
+  // Filtering data
+  const filteredRows = [];
+  for(let i = 0; i < numRows; i++) {
+    if(
+      df._data['cedolaNetta'][i] > minCedola &&
+      df._data['durata'][i] > minDurata &&
+      df._data['durata'][i] < maxDurata &&
+      df._data['prezzo'][i] < maxPrezzo &&
+      df._data['rendimentoNetto'][i] > minRendimento
+    ) {
+      const row = {};
+      Object.keys(df._data).forEach(key => {
+        row[key] = df._data[key][i];
+      });
+      filteredRows.push(row);
+    }
+  }
+
+  // Sorting by 'rendimentoNetto' in descending order
+  const sortedRows = filteredRows.sort((a, b) => b['rendimentoNetto'] - a['rendimentoNetto']);
+
+  console.log('[scrape.js] findBestBtps() function ends');
+  
+  return sortedRows;
 }
+
 
 // You may want to wrap the `handler` function call in a way that works with your specific environment.
