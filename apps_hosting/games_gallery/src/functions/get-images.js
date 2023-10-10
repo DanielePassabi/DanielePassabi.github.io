03a7fcd2c3d18efb42fb007070b958a91079194f
Daniele <daniele.passabi@hpa.ai>
2023-10-10 21:18:50 +0200

    adds try catch to avoid permission error

--- apps_hosting/games_gallery/src/functions/get-images.js
+++ apps_hosting/games_gallery/src/functions/get-images.js
@@ -2,21 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 const traverseDir = (dir, level = 0) => {
-  // Get all files and folders in directory
-  const files = fs.readdirSync(dir);
+  try {
+    // Get all files and folders in directory
+    const files = fs.readdirSync(dir);
 
-  // Log each file and folder
-  files.forEach(file => {
-    console.log('  '.repeat(level) + '├─ ' + file);
+    // Log each file and folder
+    files.forEach(file => {
+      console.log('  '.repeat(level) + '├─ ' + file);
 
-    // Check if it's a directory
-    const filePath = path.join(dir, file);
-    const stats = fs.statSync(filePath);
-    if (stats.isDirectory()) {
-      // Recurse into the folder
-      traverseDir(filePath, level + 1);
+      // Check if it's a directory
+      const filePath = path.join(dir, file);
+      const stats = fs.statSync(filePath);
+      if (stats.isDirectory()) {
+        // Recurse into the folder
+        traverseDir(filePath, level + 1);
+      }
+    });
+  } catch (error) {
+    if (error.code === 'EACCES') {
+      console.warn(`⚠️ Warning: Permission denied to access ${dir}`);
+    } else {
+      console.error(`⚠️ Error reading ${dir}: ${error.message}`);
     }
-  });
+  }
 };
 
 exports.handler = async function (event, context) {
