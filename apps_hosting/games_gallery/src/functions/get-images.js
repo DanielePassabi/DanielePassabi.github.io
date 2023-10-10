39b5c5d22a803581dd296619979b3e56a78d7596
Daniele <daniele.passabi@hpa.ai>
2023-10-10 20:46:21 +0200

    Games Gallery | Adds MORE debug logs

--- apps_hosting/games_gallery/src/functions/get-images.js
+++ apps_hosting/games_gallery/src/functions/get-images.js
@@ -1,10 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
+const traverseDir = (dir, level = 0) => {
+  // Get all files and folders in directory
+  const files = fs.readdirSync(dir);
+
+  // Log each file and folder
+  files.forEach(file => {
+    console.log('  '.repeat(level) + '├─ ' + file);
+    
+    // Check if it's a directory
+    const filePath = path.join(dir, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      // Recurse into the folder
+      traverseDir(filePath, level + 1);
+    }
+  });
+};
+
 exports.handler = async function (event, context) {
   const year = event.queryStringParameters.year;
   const dirPath = path.join(__dirname, "..", "images", year);
-  console.log("dirPath: ", dirPath)
+  
+  // Log root directory
+  console.log("Root directory: ", process.cwd());
+  
+  // Log current directory
+  console.log("Current directory: ", __dirname);
+
+  // Log directory path
+  console.log("dirPath: ", dirPath);
+
+  // Log entire directory structure starting from root
+  console.log("Entire Directory Structure:");
+  traverseDir(process.cwd());
 
   try {
     const files = fs.readdirSync(dirPath);
